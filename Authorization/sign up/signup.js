b9f594d5eca5b9aff4d5392647cf5ab0fd1abfc8
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2024-12-29 16:59:24 +0500

    register api is completed

---
+++ Authorization/sign up/signup.js
@@ -0,0 +1,38 @@
+const baseUrl = 'http://localhost:8000'
+
+const register = async () => {
+    const firstname = document.getElementById('firstname').value
+    const lastname = document.getElementById('lastname').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const confirmPass = document.getElementById('confirmPass').value
+
+    const response = await fetch(`${baseUrl}/v1/api/auth/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            firstname,
+            lastname,
+            email,
+            password,
+            confirmPass
+
+        })
+    })
+
+    const result = response.json()
+    if (result) {
+        alert("create register successful!");
+        document.getElementById('firstname').value = ''
+        document.getElementById('lastname').value = ''
+        document.getElementById('email').value = ''
+        document.getElementById('password').value = ''
+        document.getElementById('confirmPass').value = ''
+        window.location.href = '../login/index.html'
+    }
+}
+
+
